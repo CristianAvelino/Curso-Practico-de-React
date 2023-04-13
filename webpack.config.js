109128c3444362a22b26f6122782ed9149a51570
Cristian Avelino <cristian-1593@hotmail.com>
2023-04-12 20:27:07 -0500

    aplicando css y sass

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 module.exports = {
     entry: './src/index.js',
     output: {
@@ -27,6 +27,14 @@ module.exports = {
                     loader: 'html-loader'
                 }
             ]
+        },
+        {
+            test: /\.s[ac]ss$/i,
+            use:[
+                "style-loader",
+                "css-loader",
+                "sass-loader",
+            ],
         }
         ]
     },
@@ -35,5 +43,14 @@ module.exports = {
             template: './public/index.html',
             filename: './index.html'
         }),
-    ]
+        new MiniCssExtractPlugin({
+            filename: '[name].css'
+        }),
+    ],
+    devServer:{
+        
+        compress: true,
+        port: 3005,
+        static: './dist'
+    }
 }
